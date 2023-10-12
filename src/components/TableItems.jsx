f0f7a4244ea0945fdc4b0f73fb9acfe62b61ee79
Nithin <nithindevathi@gmail.com>
2023-10-12 19:06:04 +0530

    updates

--- src/components/TableItems.jsx
+++ src/components/TableItems.jsx
@@ -11,21 +11,24 @@ const TableItems = ({ data }) => {
       {/* Add more <td> elements for additional properties */}
     </tr>
   ));
-
+  const totalAmount = data.reduce((acc, val) => acc + val.amount, 0);
   return (
-    <table>
-      <thead>
-        <tr className="border border-stone-500">
-          <th className="p-2 border border-stone-500">Description</th>{" "}
-          {/* Replace with your column headers */}
-          <th className="p-2 border border-stone-500">amount</th>
-          <th className="p-2 border border-stone-500">Price</th>
-          <th className="p-2 border border-stone-500">Category</th>
-          {/* Add more <th> elements for additional headers */}
-        </tr>
-      </thead>
-      <tbody className="border border-stone-500 ">{tableRows}</tbody>
-    </table>
+    <>
+      <table>
+        <thead>
+          <tr className="border border-stone-500">
+            <th className="p-2 border border-stone-500">Description</th>{" "}
+            {/* Replace with your column headers */}
+            <th className="p-2 border border-stone-500">amount</th>
+            <th className="p-2 border border-stone-500">Price</th>
+            <th className="p-2 border border-stone-500">Category</th>
+            {/* Add more <th> elements for additional headers */}
+          </tr>
+        </thead>
+        <tbody className="border border-stone-500 ">{tableRows}</tbody>
+      </table>
+      <h1>Total Amount: {totalAmount}</h1>
+    </>
   );
 };
 
--- src/pages/AllReports.jsx
+++ src/pages/AllReports.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getExpenses } from "../Redux/ExpenseRedux";
@@ -11,23 +11,37 @@ const AllReports = () => {
   const expenseList = useSelector((state) => state.expensesReducer);
   const savingsList = useSelector((state) => state.savingsReducer);
   const incomeList = useSelector((state) => state.incomeReducer);
-
+  const [filterType, setFilter] = useState("None");
   useEffect(() => {
     dispatcher(getIncome());
     dispatcher(getSavings());
     dispatcher(getExpenses());
   }, [dispatcher]);
+
   return (
     <div className="w-fit m-auto space-x-2 space-y-4 flex flex-col justify-center items-center mt-8">
-      <div>
-        <TableItems data={expenseList.expenses}></TableItems>
-      </div>
-      <div className="text-center">
-        <TableItems data={savingsList.savings}></TableItems>
-      </div>
-      <div>
-        <TableItems data={incomeList.income}></TableItems>
-      </div>
+      <select onChange={(e) => setFilter(e.target.value)}>
+        <option>None</option>
+        <option value={"incomeVsExpense"}>Income vs Expenses</option>
+        <option value={"expense"}>Expense BreakDown</option>
+      </select>
+      {filterType == "incomeVsExpense" ? (
+        <>
+          {" "}
+          <div>
+            <TableItems data={expenseList.expenses}></TableItems>
+          </div>
+          <div>
+            <TableItems data={incomeList.income}></TableItems>
+          </div>
+        </>
+      ) : filterType == "expense" ? (
+        <div>
+          <TableItems data={expenseList.expenses}></TableItems>
+        </div>
+      ) : (
+        <h1>Select an Option Above</h1>
+      )}
     </div>
   );
 };
